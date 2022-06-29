cd71b200926ecaa653f9f456629f4b0d1f3660b7
Ashish Gupta <ashishguptaid@gmail.com>
2022-06-29 09:45:06 +0530

    Added IncrementAccountCreditLimit workflow assembly

---
+++ IncrementAccountCreditLimit/IncrementAccountCreditLimit/IncrementCrdeitLimit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+namespace IncrementAccountCreditLimit
+{
+    public class IncrementCrdeitLimit : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("Decimal Input")]
+        public InArgument<decimal> DecInput { get; set; }
+
+        [RequiredArgument]
+        [Output("Decimal Output")]
+        public OutArgument<decimal> DecOutput { get; set; }
+        protected override void Execute(CodeActivityContext context) {
+            decimal input = DecInput.Get(context);
+            DecOutput.Set(context, input + 10);
+        }
+    }
+}
